7ef4c87988024f6b801d037f6c4b566112d30520
Scharif47 <scharif47@outlook.de>
2025-01-31 22:03:01 +0100

    reusable table update

--- src/components/Table.js
+++ src/components/Table.js
@@ -1,12 +1,16 @@
-function Table({ data }) {
-  const renderedRows = data.map((fruit) => {
+function Table({ data, config, keyFn }) {
+  const renderedHeaders = config.map((column) => {
+    return <th key={column.label}>{column.label}</th>;
+  });
+
+  const renderedRows = data.map((rowData) => {
+    const renderedCells = config.map((column) => {
+      return <td key={column.label}>{column.render(rowData)}</td>;
+    });
+
     return (
-      <tr className="border-b" key={fruit.name}>
-        <td className="p-3">{fruit.name}</td>
-        <td className="p-3">
-          <div className={`p-3 m-2 ${fruit.color}`}></div>
-        </td>
-        <td className="p-3">{fruit.score}</td>
+      <tr className="border-b" key={keyFn(rowData)}>
+        {renderedCells}
       </tr>
     );
   });
@@ -14,11 +18,7 @@ function Table({ data }) {
   return (
     <table className="table-auto border-spacing-2">
       <thead>
-        <tr className="border-b-2">
-          <th>Fruit</th>
-          <th>Color</th>
-          <th>Score</th>
-        </tr>
+        <tr className="border-b-2">{renderedHeaders}</tr>
       </thead>
       <tbody>{renderedRows}</tbody>
     </table>
